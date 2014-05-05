3f3cbe745fe2bf81849b0aad2de37044cad6ec7a
Dallas Read <dallas@excitereative.ca>
2014-05-05 14:09:21 -0300

    set room.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -64,7 +64,7 @@ class RoomsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_room
-      @room = current_user.rooms.find(params[:id])
+      @room = Room.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
